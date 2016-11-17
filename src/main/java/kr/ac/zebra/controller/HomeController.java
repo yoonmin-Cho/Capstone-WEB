e08e115668cde7144645ea79d06a24d9e7f8d5cc
yoonmin-Cho <yoonmin9411@gmail.com>
2016-11-17 16:01:33 +0900

    feature

--- src/main/java/kr/ac/zebra/controller/HomeController.java
+++ src/main/java/kr/ac/zebra/controller/HomeController.java
@@ -3,7 +3,6 @@ package kr.ac.zebra.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
--- src/main/java/kr/ac/zebra/service/WebProductService.java
+++ src/main/java/kr/ac/zebra/service/WebProductService.java
@@ -1,8 +1,18 @@
 package kr.ac.zebra.service;
 
+import kr.ac.zebra.dao.WebProductDAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebProductService {
 
+	private WebProductDAO webProductDAO;
+	
+	@Autowired
+	public void setDAO(WebProductDAO webProductDAO){
+		this.webProductDAO = webProductDAO;
+	}
+	
 }
