87cb26acbd7529139394c4669838989860271d42
yoonmin <yoonmin9411@gmail.com>
2016-11-29 01:52:03 +0900

    Update AppScanController.java

--- src/main/java/kr/ac/zebra/controller/AppScanController.java
+++ src/main/java/kr/ac/zebra/controller/AppScanController.java
@@ -4,53 +4,56 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import kr.ac.zebra.dto.AppApply;
 import kr.ac.zebra.dto.Product;
 import kr.ac.zebra.dto.Review;
 import kr.ac.zebra.service.AppProductService;
 import kr.ac.zebra.service.AppReviewService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class AppScanController {
 
 	private AppProductService appProductService;
 	private AppReviewService appReviewService;
-	
+
 	@Autowired
-	public void setService(AppProductService appProductService, AppReviewService appReviewService){
-		this.appProductService = appProductService;
-		this.appReviewService = appReviewService;
+	public void setReviewService(AppReviewService reviewService, AppProductService productService) {
+		this.appReviewService = reviewService;
+		this.appProductService = productService;
 	}
-	
-	@RequestMapping(value="/appScan")
-	public String showScanPage(HttpServletRequest request){
-		
+
+	@RequestMapping("/appScan")
+	public String showScanInfo(HttpServletRequest request) {
+
 		String barcode = request.getParameter("barcode");
-		      
+
 		Product product = appProductService.getProductByBarcode(barcode);
-		List<Review> reviews = appReviewService.getReviewByBarcode(barcode);
-		    
-		if (product == null){
+		List<Review> reviews = appReviewService.getReviewsByBarcode(barcode);
+
+		if (product == null) {
 			AppApply apply = appProductService.getApply(barcode);
-		      String result = "";
-		      if (apply == null){		       
-		    	  result = "null";
-		    	  request.setAttribute("result", result);
-		      }else{
-		        result = "existApply";
-		        request.setAttribute("result", result);
-		      }		    
-		}   
-		else{
+			String result = "";
+
+			if (apply == null) {
+				result = "null";
+				request.setAttribute("result", result);
+			} else {
+				result = "existApply";
+
+				request.setAttribute("result", result);
+			}
+		} else {
 			appProductService.scanCounting(barcode);
 
-			request.setAttribute("product", product);		      
-			request.setAttribute("reviews", reviews);	
-		}   
-		return "appScan";	  
+			request.setAttribute("product", product);
+			request.setAttribute("reviews", reviews);
+		}
+
+		return "appScan";
+
 	}
 }
