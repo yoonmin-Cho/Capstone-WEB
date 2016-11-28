b866ac67187d92692a0d814aaaa923e573ec868a
yoonmin <yoonmin9411@gmail.com>
2016-11-29 01:53:43 +0900

    Update WebReviewController.java

--- src/main/java/kr/ac/zebra/controller/WebReviewController.java
+++ src/main/java/kr/ac/zebra/controller/WebReviewController.java
@@ -5,16 +5,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import kr.ac.zebra.dto.Product;
-import kr.ac.zebra.dto.Review;
-import kr.ac.zebra.service.WebProductService;
-import kr.ac.zebra.service.WebReviewService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.ac.zebra.dto.Product;
+import kr.ac.zebra.dto.Review;
+import kr.ac.zebra.service.WebProductService;
+import kr.ac.zebra.service.WebReviewService;
 
 @Controller
 public class WebReviewController {
@@ -23,70 +22,65 @@ public class WebReviewController {
 	private WebProductService webProductService;
 	
 	@Autowired
-	public void setService(WebReviewService webReviewService,WebProductService webProductService){
+	public void setService(WebReviewService webReviewService, WebProductService webProductService){
 		this.webReviewService = webReviewService;
 		this.webProductService = webProductService;
 	}
 	
-	@RequestMapping(value="/review", method=RequestMethod.GET)
-	public String showReviewPage(Model model, HttpServletRequest request, HttpSession session){
+	@RequestMapping(value="/review")
+	public String showReviewPage(HttpServletRequest request, Model model, HttpSession session){
 		
 		String barcode = request.getParameter("barcode");
-
-		//Get Product Star Point 
-		int productStarPoint = webReviewService.getStarPoint(barcode);
-		request.setAttribute("productStarPoint", productStarPoint);
 		
-		Product productInfo = webProductService.getProduct(barcode);
-		model.addAttribute("productInfo", productInfo);
-		
-		//Get Review List by review table - use barcode 
 		List<Review> reviews = webReviewService.getReviews(barcode);
 		request.setAttribute("reviews", reviews);	
 		model.addAttribute("reviews", reviews);
 		
-		//Get Star point List by review table
+		Product productInfo = webProductService.getProduct(barcode);
+		model.addAttribute("productInfo", productInfo);
+		
+		int productStarPoint = webReviewService.getStarPoint(barcode);
+		request.setAttribute("productStarPoint", productStarPoint);
+		
 		List<Integer> starPoints = webReviewService.getStarPoints(barcode);
 		request.setAttribute("starPoints", starPoints);
 		
 		//side product 
 		String category = (String)session.getAttribute("category");
-		List<Product> similarProducts = webProductService.getPopularProducts(category);
+		List<Product> similarProducts = webProductService.getPopularProduct(category);
 		request.setAttribute("similarProducts", similarProducts);
 		model.addAttribute("similarProducts", similarProducts);
-		
+			
 		return "review";
 	}
-
-	@RequestMapping(value="/reviewPurchase", method=RequestMethod.GET)
-	public String showReviewPurchasePage(Model model, HttpServletRequest request, HttpSession session){
+	
+	@RequestMapping(value="/reviewPurchase")
+	public String showReviewPurchase(HttpServletRequest request, Model model, HttpSession session){
 		
 		String barcode = request.getParameter("barcode");
-
-		//Get Product Star Point 
-		int productStarPoint = webReviewService.getStarPoint(barcode);
-		request.setAttribute("productStarPoint", productStarPoint);
+		
+		List<Review> reviews = webReviewService.getReviews(barcode);
+		request.setAttribute("reviews", reviews);	
+		model.addAttribute("reviews", reviews);
 		
 		Product productInfo = webProductService.getProduct(barcode);
 		model.addAttribute("productInfo", productInfo);
 		
 		session.setAttribute("productName", productInfo.getProductName());
 		
-		//Get Review List by review table - use barcode 
-		List<Review> reviews = webReviewService.getReviews(barcode);
-		request.setAttribute("reviews", reviews);	
-		model.addAttribute("reviews", reviews);
+		int productStarPoint = webReviewService.getStarPoint(barcode);
+		request.setAttribute("productStarPoint", productStarPoint);
 		
-		//Get Star point List by review table
 		List<Integer> starPoints = webReviewService.getStarPoints(barcode);
 		request.setAttribute("starPoints", starPoints);
 		
 		//side product 
 		String category = (String)session.getAttribute("category");
-		List<Product> similarProducts = webProductService.getPopularProducts(category);
+		List<Product> similarProducts = webProductService.getPopularProduct(category);
 		request.setAttribute("similarProducts", similarProducts);
 		model.addAttribute("similarProducts", similarProducts);
 		
 		return "reviewPurchase";
 	}
+	
 }
